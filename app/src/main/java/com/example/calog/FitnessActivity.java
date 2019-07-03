f769c7af45cba13cfa94dd832a4a11f510e00039
Namhee <leenh123@users.noreply.github.com>
2019-07-03 18:25:46 +0900

    20190703 FitnessActivity frame Namhee

--- app/src/main/java/com/example/calog/FitnessActivity.java
+++ app/src/main/java/com/example/calog/FitnessActivity.java
@@ -4,39 +4,67 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.drm.DrmStore;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class FitnessActivity extends AppCompatActivity {
     RelativeLayout btnCardioActivity, btnWeightTrainingActivity, btnStretchingActivity;
-
+    ImageButton FitnessAdd;
+    ImageView btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fitness);
 
+        btnBack=findViewById(R.id.btnBack);     //메인페이지로 이동
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FitnessActivity.this, MainHealthActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+//운동선택
+        FitnessAdd = findViewById(R.id.btnAddFitness);
+        FitnessAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FitnessActivity.this, "운동을 추가합니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+//유산소운동 데이터 출력
         btnCardioActivity=findViewById(R.id.btnCardioActivity);
         btnCardioActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(FitnessActivity.this, "유산소운동 데이터를 출력합니다.", Toast.LENGTH_SHORT).show();
+                Toast.makeText(FitnessActivity.this, "유산소운동 칼로리소모량을 출력합니다.", Toast.LENGTH_SHORT).show();
             }
         });
 
+
+//무산소운동 데이터 출력
         btnWeightTrainingActivity=findViewById(R.id.btnWeightTrainingActivity);
         btnWeightTrainingActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(FitnessActivity.this, "무산소운동 데이터를 출력합니다.", Toast.LENGTH_SHORT).show();
+                Toast.makeText(FitnessActivity.this, "무산소운동 칼로리소모량 출력합니다.", Toast.LENGTH_SHORT).show();
             }
         });
 
+//스트레칭 검색
         btnStretchingActivity=findViewById(R.id.btnStretchingActivity);
         btnStretchingActivity.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,21 +74,4 @@ public class FitnessActivity extends AppCompatActivity {
         });
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.fitnessmenu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.fitnessSelect:
-            Toast.makeText(FitnessActivity.this, "운동 선택화면으로 이동합니다.", Toast.LENGTH_SHORT).show();
-            break;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
