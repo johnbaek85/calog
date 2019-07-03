96a2cf907e337c7c6001949209637c0d02f853bd
ued123 <ued_123@naver.com>
2019-07-03 14:50:55 +0900

    19-07-03 "seongheon first DrinkingActivity"

---
+++ app/src/main/java/com/example/calog/DrinkingActivity.java
@@ -0,0 +1,13 @@
+package com.example.calog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DrinkingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drinking);
+    }
+}
