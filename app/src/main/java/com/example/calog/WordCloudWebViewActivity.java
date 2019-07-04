161227a2bb9ac359cddd65cdf34d6c968c345098
jonghwa <john.baek85@gmail.com>
2019-07-04 15:43:49 +0900

    20190704 add WordCloudActivity. jonghwa

--- app/src/main/java/com/example/calog/WordCloudWebViewActivity.java
+++ app/src/main/java/com/example/calog/WordCloudWebViewActivity.java
@@ -6,39 +6,38 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.SlidingDrawer;
-import android.widget.TextView;
 import android.widget.Toast;
 
-public class WordCloudActivity extends AppCompatActivity {
-    ImageView btnBack;
-    TextView cloudWords;
+public class WordCloudWebViewActivity extends AppCompatActivity {
+    ImageView btnBack, btnMainShortcut;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_word_cloud);
-        cloudWords = findViewById(R.id.cloudWords);
+        setContentView(R.layout.activity_word_cloud_web_view);
 
         btnBack = findViewById(R.id.btnBack);
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(WordCloudActivity.this, "이전 페이지 Activity로 이동",
+                Toast.makeText(WordCloudWebViewActivity.this, "이전 페이지 Activity로 이동",
                         Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(WordCloudActivity.this, MainHealthActivity.class);
+                Intent intent = new Intent(WordCloudWebViewActivity.this, WordCloudActivity.class);
                 startActivity(intent);
             }
         });
 
-        cloudWords.setOnClickListener(new View.OnClickListener() {
+        btnMainShortcut = findViewById(R.id.btnMAinShortcut);
+        btnMainShortcut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(WordCloudActivity.this, "관련 기사로 이동합니다.", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(WordCloudActivity.this, WordCloudWebViewActivity.class);
+                Toast.makeText(WordCloudWebViewActivity.this, "메인 페이지로 이동합니다.", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(WordCloudWebViewActivity.this, MainHealthActivity.class);
                 startActivity(intent);
             }
         });
-    }
 
+
+    }
 }
