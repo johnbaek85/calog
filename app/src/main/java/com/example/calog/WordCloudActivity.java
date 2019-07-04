2df562a827ba37253c64208dbb67537179e2cb00
jonghwa <john.baek85@gmail.com>
2019-07-04 10:32:00 +0900

    20190704 WordCloudActivity first commit jonghwa

---
+++ app/src/main/java/com/example/calog/WordCloudActivity.java
@@ -0,0 +1,17 @@
+package com.example.calog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.SlidingDrawer;
+
+public class WordCloudActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_word_cloud);
+    }
+
+}
