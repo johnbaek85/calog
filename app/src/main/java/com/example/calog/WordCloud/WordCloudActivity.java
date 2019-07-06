f700178339d63bffe0ffaef8d79d430783cbaaa5
jonghwa <john.baek85@gmail.com>
2019-07-06 17:39:14 +0900

    20190706 insert layout jonghwa

--- app/src/main/java/com/example/calog/WordCloud/WordCloudActivity.java
+++ app/src/main/java/com/example/calog/WordCloud/WordCloudActivity.java
@@ -5,12 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.calog.MainHealthActivity;
 import com.example.calog.R;
+
+import java.util.ArrayList;
+
 
 public class WordCloudActivity extends AppCompatActivity {
     ImageView btnBack;
@@ -41,6 +46,24 @@ public class WordCloudActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // 데이터 준비 ( -> 크롤링 결과를 VO 에 담아서 넘길 예정)
+        ArrayList<String> hotKeywordSample = new ArrayList<>();
+        hotKeywordSample.add("발기부전");
+        hotKeywordSample.add("조루");
+        hotKeywordSample.add("모태솔로");
+        hotKeywordSample.add("전립선염");
+        hotKeywordSample.add("1cm");
+
+
+        //어댑터 만들기
+
+        ArrayAdapter<String> wordCloudAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, hotKeywordSample);
+
+        ListView wordlist = (ListView) findViewById(R.id.WordCloudArticleList);
+        wordlist.setAdapter(wordCloudAdapter);
+
+
     }
 
 }
