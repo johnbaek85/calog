f6aa485d6b4155331b9ad692a700fe25b26a7ae0
ued123 <ued_123@naver.com>
2019-07-10 15:16:33 +0900

    seongheon
    190710
    13.0 DrinkingActivity
    leeseongheon

---
+++ app/src/main/java/com/example/calog/VO/DrinkingVO.java
@@ -0,0 +1,44 @@
+package com.example.calog.VO;
+
+import java.sql.Date;
+
+public class DrinkingVO {
+
+    private String userId;
+    private Date drinkingDate;
+    private double alcoholContent;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public Date getDrinkingDate() {
+        return drinkingDate;
+    }
+
+    public void setDrinkingDate(Date drinkingDate) {
+        this.drinkingDate = drinkingDate;
+    }
+
+    public double getAlcoholContent() {
+        return alcoholContent;
+    }
+
+    public void setAlcoholContent(double alcoholContent) {
+        this.alcoholContent = alcoholContent;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "DrinkingVO{" +
+                "userId='" + userId + '\'' +
+                ", drinkingDate=" + drinkingDate +
+                ", alcoholContent=" + alcoholContent +
+                '}';
+    }
+}
