947819f8d8beb853a0b7c6bc88906f3200d2d0ac
JiJunHyuk <52313233+JiJunHyuk@users.noreply.github.com>
2019-07-10 11:18:11 +0900

    20190710 test push junhyuk

--- app/src/main/java/com/example/calog/MainHealthActivity.java
+++ app/src/main/java/com/example/calog/MainHealthActivity.java
@@ -22,14 +22,11 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
-
 import com.example.calog.Diet.DietActivity;
 import com.example.calog.Drinking.DrinkingActivity;
 import com.example.calog.Drinking.DrinkingCheckActivity;
 import com.example.calog.Fitness.FitnessActivity;
-import com.example.calog.Sleeping.DecibelCheck.DecibelMainActivity;
+import com.example.calog.Sleeping.SleepCheckActivity;
 import com.example.calog.Sleeping.SleepingActivity;
 import com.example.calog.WordCloud.WordCloudActivity;
 import com.example.calog.signUp.MainJoinActivity;
@@ -88,7 +85,9 @@ public class MainHealthActivity extends AppCompatActivity {
                 /*Toast.makeText(MainActivity.this, "달력 Activity로 이동",
                         Toast.LENGTH_SHORT).show();*/
                 intent = new Intent(MainHealthActivity.this, CalendarActivity.class);
-                intent.putExtra("date", monthName.getText().toString());
+                intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                //변경된 현재 시간값을 가져가서 달력을 재구성한다.
+                intent.putExtra("currentSelectedTime",currentSelectedTime);
                 startActivity(intent);
             }
         });
@@ -97,8 +96,8 @@ public class MainHealthActivity extends AppCompatActivity {
         btnUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainHealthActivity.this, "로그인 Activity로 이동",
-                        Toast.LENGTH_SHORT).show();
+                /*Toast.makeText(MainHealthActivity.this, "로그인 Activity로 이동",
+                        Toast.LENGTH_SHORT).show();*/
                 Intent intent = new Intent(MainHealthActivity.this, MainJoinActivity.class);
                 startActivity(intent);
             }
@@ -182,7 +181,7 @@ public class MainHealthActivity extends AppCompatActivity {
         btnSleepStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                intent = new Intent(MainHealthActivity.this, DecibelMainActivity.class);
+                intent = new Intent(MainHealthActivity.this, SleepCheckActivity.class);
                 startActivity(intent);
             }
         });
@@ -222,14 +221,89 @@ public class MainHealthActivity extends AppCompatActivity {
                 .showMonthName(true)
                 .build();
 
-        horizontalCalendar.setCalendarListener(new HorizontalCalendarListener() {
+        //캘린더 데이터 변경
+        horizontalCalendar.setCalendarListener(new HorizontalCalendarListener()
+        {
             @Override
             public void onDateSelected(Date date, int position) {
+
                 Toast.makeText(MainHealthActivity.this,
                         DateFormat.getDateInstance().format(date) + " is selected!",
                         Toast.LENGTH_SHORT).show();
+
+                //horizontalCalendar.date
                 monthName.setText(DateFormat.getDateInstance().format(date));
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        File cropFile = screenShot;
+
+        if(requestCode ==100){
+            if (resultCode == RESULT_OK) {
+                cropFile = new File(Crop.getOutput(data).getPath());
+            }
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) { // API 24 이상 일경우..
+                uriFile = FileProvider.getUriForFile(getApplicationContext(),
+                        getApplicationContext().getPackageName() + ".provider", cropFile);
+            } else { // API 24 미만 일경우..
+                uriFile = Uri.fromFile(cropFile);
+            }
+            Intent shareIntent = new Intent();
+            shareIntent.setAction(Intent.ACTION_SEND);
+            shareIntent.putExtra(Intent.EXTRA_STREAM, uriFile);
+            shareIntent.setType("image/*");
+            startActivity(Intent.createChooser(shareIntent, "선택"));
+        }
+    }
+
+    public File ScreenShot(View view){
+        view.setDrawingCacheEnabled(true); //화면에 뿌릴때 캐시를 사용하게 한다
+        Bitmap screenBitmap = view.getDrawingCache(); //캐시를 비트맵으로 변환
+        String filename = "screenshot.png";
+        File file = new File(Environment.getExternalStorageDirectory() + "/Pictures", filename);
+
+        System.out.println("..........." + filename);
+        //Pictures폴더 screenshot.png 파일
+        FileOutputStream os = null;
+        try{
+            os = new FileOutputStream(file);
+            screenBitmap.compress(Bitmap.CompressFormat.PNG, 90, os); //비트맵을 PNG파일로 변환
+            os.close();
+        }catch (Exception e){
+            System.out.println(e.toString());
+            return null;
+        }
+        view.setDrawingCacheEnabled(false);
+        return file;
+    }
+
+    public void permissionCheck(){
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
+        }
+    }
+
+    //기존액티비티가 재실행될때
+    @Override
+    protected void onNewIntent(Intent intent)
+    {
+        super.onNewIntent(intent);
+
+        System.out.println("onNewIntent Call");
+
+        //현재 선택된 시간 가져오기
+        currentSelectedTime=intent.getLongExtra("currentSelectedTime",0);
+
+        java.sql.Date date = new java.sql.Date(currentSelectedTime);
+
+        //date.setTime(mill);
+        //시간 재설정
+        monthName.setText(DateFormat.getDateInstance().format(date));
+        horizontalCalendar.selectDate(date,true); //false는 이벤트를 주고 true는 이벤트를 주지않고 즉시 변경
     }
 }
